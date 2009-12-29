9ca88359245af778a0c1488258bc7c54bb82de20
Gabor Szabo <szabgab@gmail.com>
2009-12-29 11:00:13 +0200

    switch to use Template Toolkit and add titles to each page

--- script/build.pl
+++ script/build.pl
@@ -3,8 +3,9 @@ use strict;
 use warnings;
 
 use Cwd ();
-use HTML::Template;
 use File::Basename qw(dirname basename);
+use Template ();
+
 
 my $root = dirname(dirname(Cwd::abs_path($0)));
 unlink glob "$root/site/*.html";
@@ -12,14 +13,24 @@ unlink glob "$root/site/*.html";
 my @files = glob "$root/templates/*.tmpl";
 #print "@files";
 
+my $tt = Template->new({
+        INCLUDE_PATH => "$root/templates",
+        POST_CHOMP   => 1,                # cleanup whitespace
+        EVAL_PERL    => 0,                # evaluate Perl code blocks
+        OUTPUT_PATH  => "$root/site",
+        ENCODING     => 'utf8',
+    });
+
 foreach my $tmpl (@files) {
 	my $file = basename($tmpl);
 	next if $file eq 'nav.tmpl' or $file eq 'footer.tmpl' or $file eq 'head.tmpl';
 	print "Processing $file\n";
-	my $t = HTML::Template->new(filename => $tmpl, path => "$root/templates");
-	#$t->param();
-	$file =~ s/tmpl$/html/;
-	open my $out, '>', "$root/site/$file" or die $!;
-	print $out $t->output;
+	(my $out = $file) =~ s/tmpl$/html/;
+        my $stash = {};        
+        $tt->process(
+            $file,
+            $stash,
+            $out,
+            {binmode => ':utf8'},
+        ) or die $tt->error . " in $tmpl";
 }
-
